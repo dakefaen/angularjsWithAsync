8d2e8764f66477005d959b58cfc9b80ba3eec9d3
yguan <yguan@slb.com>
2018-06-21 18:08:41 +0800

    add more examples

--- client/app.js
+++ client/app.js
@@ -6,15 +6,38 @@ require('babel-polyfill');
 
     myControllerConstrut.$inject = ['$q', '$scope', '$http'];
     function myControllerConstrut($q, $scope, $http) {
-
+        var ctrl = this;
+        ctrl.hello = 'hello';
         $scope.greetings = 'hello world!';
+
         $scope.query = async () => {
             $scope.greetings = 'query...';
             let response = await $http.get('/api');
             $scope.greetings = response.data;
             // $scope.$digest();
             $scope.$apply();
+        };
+
+        async function queryAlot() {
+            let response = await $http.get('/api');
+            let response2 = await $http.get('/api2');
+            let val = response2.data + "-" + response.data;
+            ctrl.hello = val;
+        }
+
+        $scope.query2 = () => {
+            ctrl.hello = 'query2...';
+            queryAlot().finally(() => {
+                $scope.$digest();
+                // $scope.$apply();
+            });
+        }
+
+        $scope.query3 = () => {
+            ctrl.hello = 'query3...';
+            $q.when(0).then(queryAlot).finally(() => {
+                $scope.greetings = "3 done";
+            });
         }
     }
-
 })(angular);
--- server/server.js
+++ server/server.js
@@ -10,9 +10,16 @@ app.get('/', function (req, res) {
 
 app.get('/api', function (req, res) {
     let response = 0;
-    for (let i = 0; i < 1e8; i++) {
+    for (let i = 1; i < 5e8; i++) {
         response += i;
+    }
+    res.send(response.toString());
+});
 
+app.get('/api2', function (req, res) {
+    let response = 1;
+    for (let i = 1; i < 5e8; i++) {
+        response += i;
     }
     res.send(response.toString());
 });
