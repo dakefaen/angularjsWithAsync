cc61da0fe320cc9bb82e3554248341f101ce8e00
yguan <yguan@slb.com>
2018-06-27 15:45:03 +0800

    add tests for async&await

--- client/test/app.spec.js
+++ client/test/app.spec.js
@@ -1,18 +1,18 @@
 describe('app.spec.js', () => {
     beforeEach(module('demoApp'));
 
-    var controller, scope, http;
+    var controller, scope, http, q;
     var mockData = { data: "mock data" };
 
-    beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+    beforeEach(inject(function ($controller, $rootScope, $httpBackend, $q) {
         scope = $rootScope.$new();
         controller = $controller('myController', { $scope: scope });
+        q = $q;
 
         http = $httpBackend;
         http.whenGET('/api').respond(mockData);
         http.whenGET('/api2').respond(mockData);
     }));
-
 
     it("should init correctly", () => {
         expect(scope.greetings).toEqual('hello world!');
@@ -32,6 +32,7 @@ describe('app.spec.js', () => {
             controller.queryAlot = () => {
                 controller.hello = mockData.data;
                 return Promise.resolve(1);
+                // return q.when(1);
             };
         });
 
@@ -53,5 +54,21 @@ describe('app.spec.js', () => {
     });
 
 
+    function resolveAfter2Seconds(x) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(x);
+            }, 2000);
+        });
+    }
 
+    async function add1(x) {
+        var a = resolveAfter2Seconds(20);
+        var b = resolveAfter2Seconds(30);
+        return x + await a + await b;
+    }
+    it('tests that async / await works', async () => {
+        var v = await add1(10);
+        expect(v).toBe(60);
+    });
 });
